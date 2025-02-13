a184473f6693deade6e896a18f2e36107a30cd19
gabrieldsvr <gabriel.vargas@paripassu.com.br>
2025-02-13 14:33:42 -0300

    .env

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 // Configuração da conexão com o banco de dados
-const sequelize = new Sequelize('ichurch', 'root', '31310801gG@', {
-    host: 'localhost',
-    dialect: 'mysql',
-    logging: false, // Para evitar logs SQL no console
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        logging: process.env.DB_LOGGING === 'true', // Converte string para boolean
+    }
+);
 
 // Testar conexão com o banco
 sequelize

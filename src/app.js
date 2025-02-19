9422aa0b496a3775c7a99114abce9cae8e8cea6f
gabrieldsvr <gabriel.vargas@paripassu.com.br>
2025-02-19 17:52:59 -0300

    Ajustando community

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const cors = require('cors');
 const membersRoutes = require('./routes/membersRoutes');
 const regularAttendeesRoutes = require('./routes/regularAttendeesRoutes');
 const visitorsRoutes = require('./routes/visitorsRoutes');
-const eventsRoutes = require('./routes/eventsRoutes');
-const attendanceRoutes = require('./routes/attendanceRoutes');
+const eventsRoutes = require('./routes/community/eventsRoutes');
+const attendanceRoutes = require('./routes/community/attendanceRoutes');
 const reportsRoutes = require('./routes/reportsRoutes');
 const utilityRoutes = require('./routes/utilityRoutes');
 const leadershipRoutes = require('./routes/leadershipRoutes');
@@ -13,6 +13,7 @@ const usersRoutes = require('./routes/sca/usersRoutes');
 const companiesRoutes = require('./routes/sca/companiesRoutes');
 const rolesRoutes = require('./routes/sca/rolesRoutes');
 const permissionsRoutes = require('./routes/sca/permissionsRoutes');
+const peopleRoutes = require('./routes/community/PeopleRoutes');
 
 const app = express();
 app.use(cors());
@@ -22,13 +23,14 @@ app.use(express.json());
 app.use('/members', membersRoutes);
 app.use('/regular-attendees', regularAttendeesRoutes);
 app.use('/visitors', visitorsRoutes);
-app.use('/events', eventsRoutes);
-app.use('/attendance', attendanceRoutes);
 app.use('/reports', reportsRoutes);
 app.use('/utility', utilityRoutes);
 app.use('/leaders', leadershipRoutes);
 
-// app.use('/community/people', peopleRoutes);
+app.use('/community/people', peopleRoutes);
+app.use('/community/events', eventsRoutes);
+app.use('/community/attendance', attendanceRoutes);
+
 
 app.use("/sca/auth", authRoutes);
 app.use("/sca/users", usersRoutes);

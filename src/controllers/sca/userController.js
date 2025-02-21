09d23b0d32bc9f710aed92c2465deef710c4a9c5
gabrieldsvr <gabriel.vargas@paripassu.com.br>
2025-02-20 22:15:36 -0300

    sistema de ministerios

--- src/controllers/sca/userController.js
+++ src/controllers/sca/userController.js
@@ -1,18 +1,39 @@
 const bcrypt = require("bcryptjs");
 const { Users, Companies } = require("../../models/sca");
 const {Logger} = require("../../service/logService");
+const {People} = require("../../models/community");
+
 
 exports.createUser = async (req, res) => {
     try {
-        const { name, email, password, company_id, is_master } = req.body;
+        const { name, email, password, is_master, person_id } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
 
         // Criar usuário normalmente
-        const user = await Users.create({ name, email, password: hashedPassword, company_id, is_master});
+        const user = await Users.create({
+            name,
+            email,
+            password: hashedPassword,
+            company_id: req.user.company_id,
+            is_master,
+        });
 
         // Se for o primeiro usuário (master), atualizar a empresa com o owner_id
         if (is_master) {
-            await Companies.update({ owner_id: user.id }, { where: { id: company_id } });
+            await Companies.update({ owner_id: user.id }, { where: { id: req.user.company_id } });
+        }
+
+        // Atualizar a tabela People com o user_id
+        if (person_id) {
+            const person = await People.findOne({
+                where: { id: person_id, company_id: req.user.company_id },
+            });
+
+            if (!person) {
+                return res.status(404).json({ error: "Pessoa não encontrada." });
+            }
+
+            await People.update({ user_id: user.id }, { where: { id: person_id } });
         }
 
         res.status(201).json({ message: "Usuário criado com sucesso.", user });

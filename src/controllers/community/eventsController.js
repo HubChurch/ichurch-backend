02dbf9012a19c8796d5b49c9d07f65086198fe5d
gabriel.vargas <gabriel.vargas@paripassu.com.br>
2025-06-10 11:52:40 -0300

    versao 0.8.18 -Ajuste retorno get event

--- src/controllers/community/eventsController.js
+++ src/controllers/community/eventsController.js
@@ -97,13 +97,18 @@ exports.getEventById = async (req, res) => {
                 company_id: req.user.company_id,
             },
         });
-        if (!event)
-            return res.status(404).json({ error: "Evento não encontrado." });
-        res.json(event);
+
+        if (!event) {
+            return res.status(404).json({ message: "Evento não encontrado." });
+        }
+
+        return res.status(200).json(event);
     } catch (err) {
-        res.status(500).json({ error: "Erro ao buscar evento." });
+        console.error("Erro ao buscar evento:", err);
+        return res.status(500).json({ message: "Erro interno ao buscar o evento." });
     }
 };
+
 
 // 📌 Atualizar evento
 exports.updateEvent = async (req, res) => {
